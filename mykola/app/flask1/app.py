cf04956c9e0a32379b6089b2fcea1f9e29ab9868
Msrt12 <110978163+Msrt12@users.noreply.github.com>
2026-01-27 12:12:37 +0200

    Change default name in hello function

--- mykola/app/flask1/app.py
+++ mykola/app/flask1/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello(name="DevOps engineer Mykola"):
+def hello(name="This is photo of a bird"):
     return render_template('index.html', person=name)
 
 if __name__ == "__main__":
