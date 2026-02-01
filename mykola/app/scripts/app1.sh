1df9b0de85a76b224f1545ee9b6b5f8eb84dc6d2
Msrt12 <110978163+Msrt12@users.noreply.github.com>
2026-02-01 16:23:15 +0200

    Used "tee -a" for stdout

--- mykola/app/scripts/app1.sh
+++ mykola/app/scripts/app1.sh
@@ -7,6 +7,6 @@ if cd "$APP_DIR"; then
     source venv/bin/activate
     python3 app.py
 else
-    echo "Project directory not found!" >> $LOGS
+    echo "Project directory not found!" | tee -a "$LOGS"
     exit 1
 fi
